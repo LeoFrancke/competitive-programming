fa038b8066cca3af7c9f5e96d81b4e109ef7104d
Leandro Francke <219414135+leofrancke@users.noreply.github.com>
2026-08-11 06:38:41 -0300

    bugfix: C++ swap

--- codeforces/2026/236A-boy-or-girl.cpp
+++ codeforces/2026/236A-boy-or-girl.cpp
@@ -4,15 +4,20 @@
 // avoiding the above code for now, just to get a feel of the language
 
 int main() {
-    // first day trying the language; please don't laugh at my code
-    std::vector<set> username;
+    std::string username;
     std::cin >> username;
 
-    if username.size() % 2 == 0 {
-        std::cout << "CHAT WITH HER!";
-    } else {
-        std::cout << "IGNORE HIM!";
+    std::set<char> unique_chars;
+    for (unsigned int i = 0; i < username.size(); i++) {
+        unique_chars.insert(username[i]);
     }
 
+    if (unique_chars.size() % 2 == 0) {
+        std::cout << "CHAT WITH HER!" << "\n";
+    } else {
+        std::cout << "IGNORE HIM!" << "\n";
+    }
+
+    return 0;
 }
 
