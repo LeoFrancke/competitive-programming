154890ff0910bf39a9d7c6a9cd66515c8e2ab063
Leandro Francke <219414135+leofrancke@users.noreply.github.com>
2026-08-17 07:31:38 -0300

    refactor: rename x to score for readability

--- codeforces/2026/158A-next-round.cpp
+++ codeforces/2026/158A-next-round.cpp
@@ -5,31 +5,33 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    // n: number of total participants
+    // k: cut-off score's place
     int n, k;
     cin >> n >> k;
 
     int counter = 0;
-    int nota_de_corte = 1;
+    int cut_off_score = 1;
 
-    int x;
     vector<int> scores;
-    for (int i = 0; i < n; i++) {
-        cin >> x;
-        scores.push_back(x);
+    int score = 0;
+    for (int i = 0; i < n; i++) { // for # of participants
+        cin >> score;
+        scores.push_back(score);
 
-        if ((i + 1 == k) and (x > 0)) {
-            nota_de_corte = x;
+        if ((i + 1 == k) and (score > 0)) {
+            cut_off_score = score;
         }
     }
 
-    for (unsigned int i = 0; i < scores.size(); i++) {
-        if (nota_de_corte <= scores[i]) {
+    // count how many participants got score >= cut_off_score
+    for (size_t i = 0; i < scores.size(); i++) {
+        if (scores[i] >= cut_off_score) {
             counter += 1;
         }
     }
 
     cout << counter << "\n";
-
     return 0;
 }
 
